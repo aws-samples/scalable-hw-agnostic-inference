09e41e0879541c68a292a2484103f95ff125c4fd
Yahav Biran <yahavb@me.com>
2024-05-07 10:33:26 -0700

    cast num of inf with in in gradio

--- app/run-gradio.py
+++ app/run-gradio.py
@@ -11,7 +11,7 @@ pod_name=os.environ['POD_NAME']
 model_id=os.environ['MODEL_ID']
 device=os.environ["DEVICE"]
 compiled_model_id=os.environ['COMPILED_MODEL_ID']
-num_inference_steps=os.environ['NUM_OF_RUNS_INF']
+num_inference_steps=int(os.environ['NUM_OF_RUNS_INF'])
 
 
 # Define datatype
