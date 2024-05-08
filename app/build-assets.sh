f9e044afe4fae1dc02b47e62fef63881013d19e0
Yahav Biran <yahavb@me.com>
2024-05-08 15:29:00 -0700

    update 1.13.1-neuronx-py310-sdk2.18.2-ubuntu20.04

--- app/build-assets.sh
+++ app/build-assets.sh
@@ -6,7 +6,7 @@ DLC_NEURON_TAG="1.13.1-neuronx-py310-sdk2.18.2-ubuntu20.04"
 DLC_NEURON_ECR="763104351884.dkr.ecr.us-west-2.amazonaws.com"
 if [ "$IMAGE_TAG" == "amd64-neuron" ]; then
   docker logout
-  aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $DLC_NEURON_ECR
+  aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $DLC_NEURON_ECR/$DLC_NEURON_IMAGE:$DLC_NEURON_TAG
   #docker pull 763104351884.dkr.ecr.us-west-2.amazonaws.com/pytorch-inference-neuronx:1.13.1-neuronx-py310-sdk2.18.2-ubuntu20.04
   docker pull $DLC_NEURON_ECR/$DLC_NEURON_IMAGE:$DLC_NEURON_TAG
   dlc_xla_image_id=$(docker images | grep $DLC_ECR_ACCOUNT | grep $DLC_NEURON_IMAGE | awk '{print $3}')
@@ -20,7 +20,7 @@ DLC_CUDA_TAG="2.1.0-gpu-py310-cu118-ubuntu20.04-ec2"
 DLC_CUDA_ECR="763104351884.dkr.ecr.us-east-1.amazonaws.com"
 if [ "$IMAGE_TAG" == "amd64-cuda" ]; then
   docker logout
-  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DLC_CUDA_ECR
+  aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DLC_CUDA_ECR/$DLC_CUDA_IMAGE:$DLC_CUDA_TAG
   #docker pull 763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-inference:2.0.1-gpu-py310-cu121-ubuntu20.04-ec2
   docker pull $DLC_CUDA_ECR/$DLC_CUDA_IMAGE:$DLC_CUDA_IMAGE
   dlc_cuda_image_id=$(docker images | grep $DLC_ECR_ACCOUNT | grep $DLC_CUDA_TAG | awk '{print $3}')
