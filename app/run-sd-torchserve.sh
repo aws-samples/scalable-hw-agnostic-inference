74e14efa4d15840c9223493d3ad71aa373eb2c84
Yahav Biran <yahavb@me.com>
2024-05-17 12:46:54 -0700

    torchserve for xla and gpu

--- app/run-sd-torchserve.sh
+++ app/run-sd-torchserve.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 
 pip install --upgrade pip
-pip install environment_kernels
-pip install diffusers transformers accelerate safetensors matplotlib Pillow ipython torch -U
-pip install click nvitop
-mkdir model-store
-torch-model-archiver --model-name stable-diffusion --version 2.1 --handler run-sd-torchserve.py
-mv stable-diffusion.mar /model-store
-torchserve --start --ts-config config.properties
-while true; do sleep 1000; done
+if [ "$(uname -i)" = "x86_64" ]; then
+  if [ "$DEVICE" == "xla" ]; then
+    pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
+    pip install "optimum[neuronx, diffusers]"
+  elif [ "$DEVICE" == "cuda" ]; then
+    pip install environment_kernels
+    pip install diffusers transformers accelerate safetensors matplotlib Pillow ipython torch -U
+    pip install click nvitop
+  fi
+  mkdir model-store
+  torch-model-archiver --model-name stable-diffusion --version 2.1 --handler run-sd-torchserve.py
+  mv stable-diffusion.mar /model-store
+  torchserve --start --ts-config config.properties
+fi
