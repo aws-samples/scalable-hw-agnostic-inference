499922fd07d6273b1351705c9e3c252ce18b50ee
Yahav Biran <yahavb@me.com>
2024-05-17 11:17:19 -0700

    renable torch.compile for gpu4

--- app/run-sd-torchserve.py
+++ app/run-sd-torchserve.py
@@ -28,8 +28,7 @@ class DiffusersHandler(BaseHandler, ABC):
 
   def initialize(self, ctx):
     self.manifest = ctx.manifest
-    worker_id = self.ctx.worker_id
-    print("initialize worked_id:",worker_id,flush=True)
+    print("initialize ctx:",str(ctx),flush=True)
     if device=='xla':
       self.pipe = NeuronStableDiffusionPipeline.from_pretrained(compiled_model_id)
     elif device=='cuda':
