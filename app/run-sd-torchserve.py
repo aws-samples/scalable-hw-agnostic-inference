9873bcaa10f0daf42b1fb9d8d8f490e9cf798cc6
Yahav Biran <yahavb@me.com>
2024-05-17 21:33:22 -0700

    torchserve for xla and gpu5

--- app/run-sd-torchserve.py
+++ app/run-sd-torchserve.py
@@ -83,7 +83,7 @@ class DiffusersHandler(BaseHandler, ABC):
       raise Exception(f"Worker is not initialized yet.")
     print("handle data:",str(data),flush=True)
     prompt=self.preprocess(data[0])
-    model_args={'prompt': inputs,'num_inference_steps': num_inference_steps,}
+    model_args={'prompt': prompt,'num_inference_steps': num_inference_steps,}
     print("inference with model args:",str(model_args),flush=True)
     inference = self.pipe(**model_args).images[0]
     return self.postprocess(inference)
