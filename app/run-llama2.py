179ab8385eb411d1bb642caddc861762b7706d2e
Yahav Biran <yahavb@me.com>
2024-08-09 10:51:30 -0700

    llama2-cuda-perf

--- app/run-llama2.py
+++ app/run-llama2.py
@@ -22,7 +22,8 @@ if device=='xla':
   model = NeuronModelForCausalLM.from_pretrained(model_id)
 elif device=='cuda': 
   #model = AutoModelForCausalLM.from_pretrained(model_id,load_in_8bit=True,device_map="auto")
-  model = AutoModelForCausalLM.from_pretrained(model_id,load_in_8bit=True,device_map = 'cuda')
+  quantization_config = BitsAndBytesConfig(llm_int8_threshold=200.0,load_in_8bit=True)
+  model = AutoModelForCausalLM.from_pretrained(model_id,torch_dtype=torch.float16,device_map = 'auto',quantization_config=quantization_config,)
 
 tokenizer = AutoTokenizer.from_pretrained("NousResearch/Llama-2-13b-chat-hf")
 
