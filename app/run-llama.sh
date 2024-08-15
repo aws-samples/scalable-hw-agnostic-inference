3665737a0dfa805c6d025a8eab3c68eba82d1738
Yahav Biran <yahavb@me.com>
2024-08-14 22:45:03 -0700

    removeinit

--- app/run-llama.sh
+++ app/run-llama.sh
@@ -5,9 +5,9 @@ if [ "$(uname -i)" = "x86_64" ]; then
     git clone https://github.com/huggingface/optimum-neuron.git
     cd optimum-neuron
     pip install .
+    cd ..
   elif [[ "$DEVICE" == "cuda" ]]; then
     pip install nvitop bitsandbytes accelerate protobuf --no-cache-dir transformers sentencepiece
   fi
-  cd ..
   uvicorn run-llama:app --host=0.0.0.0
 fi
