fa46d6309d6a8522c5b0fdcdfa1c6562b1a3b4cc
Jonathan Wong <jonrwong@amazon.com>
2024-10-24 14:46:01 -0700

    Removing matplotlib image for bert and llama, adding pip install to sd shell file

--- app/run-sd.sh
+++ app/run-sd.sh
@@ -4,6 +4,7 @@ if [ "$(uname -i)" = "x86_64" ]; then
   if [ "$DEVICE" == "xla" ]; then
     pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
     pip install "optimum[neuronx, diffusers]"
+    pip install matplotlib
   elif [[ "$DEVICE" == "cuda" || "$DEVICE" == "triton" ]]; then
     pip install environment_kernels
     pip install diffusers transformers accelerate safetensors matplotlib Pillow ipython torch -U
