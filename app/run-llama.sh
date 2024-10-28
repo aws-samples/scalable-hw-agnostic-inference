ab03a1752493e4402571568203603ddf0dd5c9f6
Jonathan Wong <jonrwong@amazon.com>
2024-10-28 10:32:44 -0700

    Removing aarch64

--- app/run-llama.sh
+++ app/run-llama.sh
@@ -11,9 +11,11 @@ if [ "$(uname -i)" = "x86_64" ]; then
   elif [[ "$DEVICE" == "cuda" ]]; then
     pip install nvitop bitsandbytes accelerate protobuf --no-cache-dir transformers sentencepiece
   fi
-elif [ "$(uname -i)" = "aarch64" ]; then
-  if [ "$DEVICE" == "cpu" ]; then
+elif [ "$DEVICE" == "cpu" ]; then
     python3 -m pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 diffusers transformers accelerate protobuf --no-cache-dir transformers sentencepiece
-  fi
+# elif [ "$(uname -i)" = "aarch64" ]; then
+#   if [ "$DEVICE" == "cpu" ]; then
+#     python3 -m pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 diffusers transformers accelerate protobuf --no-cache-dir transformers sentencepiece
+#   fi
 fi
 uvicorn run-llama:app --host=0.0.0.0
