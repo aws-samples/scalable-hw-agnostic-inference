1cdba4852b631de3de85d10d19f6baa395c00a63
Yahav <yahavb@amazon.com>
2024-12-26 09:13:18 -0800

    flux_gradio_many_res

--- app/flux_gradio.py
+++ app/flux_gradio.py
@@ -9,35 +9,49 @@ import base64
 app = FastAPI()
 
 model_id=os.environ['MODEL_ID']
-model_api_host=os.environ['FLUX_NEURON_256X144_MODEL_API_SERVICE_HOST']
-model_api_port=os.environ['FLUX_NEURON_256X144_MODEL_API_SERVICE_PORT']
-MODEL_API_URL = f"http://{model_api_host}:{model_api_port}/generate"
+
+model_1='256x144'
+model_api_host1=os.environ['FLUX_NEURON_256X144_MODEL_API_SERVICE_HOST']
+model_api_port1=os.environ['FLUX_NEURON_256X144_MODEL_API_SERVICE_PORT']
+MODEL_API_URL1 = f"http://{model_api_host}:{model_api_port}/generate"
+
+model_2='1024x576'
+model_api_host2=os.environ['FLUX_NEURON_1024X576_MODEL_API_SERVICE_HOST']
+model_api_port2=os.environ['FLUX_NEURON_1204X576_MODEL_API_SERVICE_PORT']
+MODEL_API_URL2 = f"http://{model_api_host}:{model_api_port}/generate"
 
 def call_model_api(prompt, num_inference_steps):
+    results = {}
     try:
-        # Prepare the request payload
-        payload = {
-            "prompt": prompt,
-            "num_inference_steps": int(num_inference_steps)
-        }
+       for idx, (model_i, url) in enumerate([
+         (model_1,MODEL_API_URL1),
+         (model_2,MODEL_API_URL2)
+       ],start=1):
+         try:
+           payload = {
+             "prompt": prompt,
+             "num_inference_steps": int(num_inference_steps)
+           }
         
-        # Send POST request to the FastAPI Model Pipeline
-        response = requests.post(MODEL_API_URL, json=payload)
-        response.raise_for_status()  # Raise an error for bad status codes
+           response = requests.post(MODEL_API_URL, json=payload)
+           response.raise_for_status()  # Raise an error for bad status codes
+           data = response.json()
+           image_bytes = base64.b64decode(data['image']) 
+           image = Image.open(io.BytesIO(image_bytes))
         
-        data = response.json()
-        image_bytes = base64.b64decode(data['image']) 
-        image = Image.open(io.BytesIO(image_bytes))
-        
-        execution_time = data['execution_time']
-        
-        return image, f"{execution_time:.2f} seconds"
+           execution_time = data['execution_time']
+           results[f"image_{idx}"] = image
+           results[f"time_{idx}"] = f"{execution_time:.2f} seconds" 
     except requests.exceptions.RequestException as e:
-        # Handle request errors
-        return None, f"Request Error: {str(e)}"
+        results[f"image_{idx}"] = None
+        results[f"time_{idx}"] = f"Request Error: {str(e)}"
     except Exception as e:
-        # Handle other errors
-        return None, f"Error: {str(e)}"
+        results[f"image_{idx}"] = None
+        results[f"time_{idx}"] = f"Error: {str(e)}"
+    return (
+      results.get("image_1"),results.get("time_1"),
+      results.get("image_2"),results.get("time_2")
+    )
 
 @app.get("/health")
 def healthy():
@@ -50,14 +64,17 @@ def ready():
 interface = gr.Interface(
     fn=call_model_api,
     inputs=[
-        gr.Textbox(label="Prompt", lines=2, placeholder="Enter your prompt here..."),
+        gr.Textbox(label="Prompt", lines=1, placeholder="Enter your prompt here..."),
         gr.Number(label="Inference Steps", value=10, precision=0, 
                   info="Enter the number of inference steps; higher number takes more time but produces better image")
     ],
     outputs=[
-        gr.Image(height=512, width=512), 
-        gr.Textbox(label="Execution Time")
+        gr.Image(label=f"Image from {model_id_1}", height=256, width=144),
+        gr.Textbox(label=f"Execution Time ({model_id_1})"),
+        gr.Image(label=f"Image from {model_id_2}", height=1024, width=576),
+        gr.Textbox(label=f"Execution Time ({model_id_2})"),
     ],
+    layout="vertical",
     title=f"Image Generation via {model_id} Pipeline",
     description="Enter a prompt and specify the number of inference steps to generate an image using the model pipeline."
 )
