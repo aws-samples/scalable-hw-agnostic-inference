8fd54e3618e5e33627b16fa9a7aec4b4a3951f97
Yahav <yahavb@amazon.com>
2025-04-28 14:11:24 -0700

    flux_model_api.py

--- app/flux_model_api.py
+++ app/flux_model_api.py
@@ -55,13 +55,13 @@ def cw_pub_metric(metric_name,metric_value,metric_unit):
 TEXT_ENCODER_PATH = os.path.join(
     COMPILER_WORKDIR_ROOT,
     'text_encoder_1/compiled_model/model.pt')
-TEXT_ENCODER_2_PATH = os.path.join(
-    COMPILER_WORKDIR_ROOT,
-    'text_encoder_2/compiled_model/model.pt')
 VAE_DECODER_PATH = os.path.join(
     COMPILER_WORKDIR_ROOT,
     'decoder/compiled_model/model.pt')
 
+TEXT_ENCODER_2_DIR = os.path.join(
+    COMPILER_WORKDIR_ROOT,
+    'text_encoder_2/compiled_model/text_encoder_2')
 EMBEDDERS_DIR = os.path.join(
     COMPILER_WORKDIR_ROOT,
     'transformer/compiled_model/embedders')
@@ -77,6 +77,24 @@ TRANSFORMER_BLOCKS_DIR = os.path.join(
 
 # Login to Hugging Face
 login(hf_token, add_to_git_credential=True)
+
+class CustomFluxPipeline(FluxPipeline):
+    @property
+    def _execution_device(self):
+        return torch.device("cpu")
+
+class TextEncoder2Wrapper(nn.Module):
+    def __init__(self, sharded_model,dtype=torch.bfloat16):
+        super().__init__()
+        self.sharded_model = sharded_model
+        self.dtype = dtype
+
+    def forward(self, input_ids, output_hidden_states=False, **kwargs):
+        attention_mask = (input_ids != 0).long()
+        output = self.sharded_model(input_ids,attention_mask)
+        last_hidden_state = output[0]
+        processed_output = last_hidden_state
+        return (processed_output,)
 
 class GenerateImageRequest(BaseModel):
     prompt: str
@@ -193,28 +211,93 @@ class NeuronFluxT5TextEncoderModel(nn.Module):
     def forward(self, emb, output_hidden_states):
         return torch.unsqueeze(self.encoder(emb)["last_hidden_state"], 1)
 
+def benchmark(n_runs, test_name, model, model_inputs):
+    if not isinstance(model_inputs, tuple):
+        model_inputs = model_inputs
+
+    warmup_run = model(**model_inputs)
+
+    latency_collector = LatencyCollector()
+
+    for _ in range(n_runs):
+        latency_collector.pre_hook()
+        res = model(**model_inputs)
+        image=res.images[0]
+        #image.save(os.path.join("/tmp", "flux-dev.png"))
+        latency_collector.hook()
+    p0_latency_ms = latency_collector.percentile(0) * 1000
+    p50_latency_ms = latency_collector.percentile(50) * 1000
+    p90_latency_ms = latency_collector.percentile(90) * 1000
+    p95_latency_ms = latency_collector.percentile(95) * 1000
+    p99_latency_ms = latency_collector.percentile(99) * 1000
+    p100_latency_ms = latency_collector.percentile(100) * 1000
+
+    report_dict = dict()
+    report_dict["Latency P0"] = f'{p0_latency_ms:.1f}'
+    report_dict["Latency P50"]=f'{p50_latency_ms:.1f}'
+    report_dict["Latency P90"]=f'{p90_latency_ms:.1f}'
+    report_dict["Latency P95"]=f'{p95_latency_ms:.1f}'
+    report_dict["Latency P99"]=f'{p99_latency_ms:.1f}'
+    report_dict["Latency P100"]=f'{p100_latency_ms:.1f}'
+
+    report = f'RESULT FOR {test_name}:'
+    for key, value in report_dict.items():
+        report += f' {key}={value}'
+    print(report)
+    return report
+
+class LatencyCollector:
+    def __init__(self):
+        self.start = None
+        self.latency_list = []
+
+    def pre_hook(self, *args):
+        self.start = time.time()
+
+    def hook(self, *args):
+        self.latency_list.append(time.time() - self.start)
+
+    def percentile(self, percent):
+        latency_list = self.latency_list
+        pos_float = len(latency_list) * percent / 100
+        max_pos = len(latency_list) - 1
+        pos_floor = min(math.floor(pos_float), max_pos)
+        pos_ceil = min(math.ceil(pos_float), max_pos)
+        latency_list = sorted(latency_list)
+        return latency_list[pos_ceil] if pos_float - pos_floor > 0.5 else latency_list[pos_floor]
+
 # Load the model pipeline
 def load_model():
-    pipe = FluxPipeline.from_pretrained(
-        "black-forest-labs/FLUX.1-dev",
-        torch_dtype=DTYPE)
-    with torch_neuronx.experimental.neuron_cores_context(start_nc=0):
+    with torch_neuronx.experimental.neuron_cores_context(start_nc=8):
+       pipe = CustomFluxPipeline.from_pretrained(
+            "black-forest-labs/FLUX.1-dev",
+            torch_dtype=torch.bfloat16)
+
+    with torch_neuronx.experimental.neuron_cores_context(start_nc=8):
         pipe.text_encoder = NeuronFluxCLIPTextEncoderModel(
             pipe.text_encoder.dtype,
             torch.jit.load(TEXT_ENCODER_PATH))
+
     with torch_neuronx.experimental.neuron_cores_context(start_nc=8):
-        pipe.text_encoder_2 = NeuronFluxT5TextEncoderModel(
-            pipe.text_encoder_2.dtype,
-            torch.jit.load(TEXT_ENCODER_2_PATH))
+        pipe.vae.decoder = torch.jit.load(VAE_DECODER_PATH)
+
+    with torch_neuronx.experimental.neuron_cores_context(start_nc=0, nc_count=8):
+        sharded_text_encoder_2 = neuronx_distributed.trace.parallel_model_load(TEXT_ENCODER_2_DIR)
+        pipe.text_encoder_2 = TextEncoder2Wrapper(sharded_text_encoder_2)
+
     pipe.transformer = NeuronFluxTransformer2DModel(
         pipe.transformer.config,
         pipe.transformer.x_embedder,
         pipe.transformer.context_embedder)
-    with torch_neuronx.experimental.neuron_cores_context(start_nc=8):
-        pipe.vae.decoder = torch.jit.load(VAE_DECODER_PATH)
     return pipe
 
 model = load_model()
+#Warmup
+prompt= "A cat holding a sign that says hello world"
+num_inference_steps=10
+model_inputs={'prompt':prompt,'height':height,'width':width,'max_sequence_length':max_sequence_length,'num_inference_steps': num_inference_steps,'guidance_scale':guidance_scale}
+test_name=f"flux1-dev-50runs with dim {height}x{width} on {nodepool};num_inference_steps:{num_inference_steps}"
+benchmark(10,test_name,model,model_inputs)
 
 # Define the image generation endpoint
 @app.post("/generate", response_model=GenerateImageResponse)
